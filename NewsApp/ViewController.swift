ffc6642f1072f9071680db8653d5be7460b4d371
Johann <casiquejohann@gmail.com>
2020-03-30 23:07:01 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by johann casique on 30/03/2020.
+//  Copyright © 2020 johann casique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
